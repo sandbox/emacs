79c0e147b8b4256f27a89a4a971a3d07c27465f6
John Le <john@doloreslabs.com>
2011-02-05 03:07:55 -0800

    snippet extensions

--- settings/yasnippets.el
+++ settings/yasnippets.el
@@ -23,3 +23,41 @@
                                     try-complete-lisp-symbol-partially
                                     try-complete-lisp-symbol
                                     )))
+
+(defun insert_yardoc_comments_for_arguments ()
+  (save-excursion
+    (re-search-forward "def \\([a-zA-Z_0-9.?!]+\\)" nil t)
+    (match-data)
+    (let ((method-name (buffer-substring (match-beginning 1) (match-end 1)))
+          ;; this assumes that ruby methods are defined on a single line
+          (arguments-string (buffer-substring (match-end 1) (line-end-position))))
+      (let ((args-list
+             (remove-if
+              (lambda (x) (string= x ""))
+              (mapcar
+               (lambda (arg) (car (split-string (replace-regexp-in-string "[ ()]" "" arg) "=")))
+               (split-string arguments-string ",")))))
+        (apply 'concat (mapcar (lambda (arg) (concat "\n# @param [${type}] " arg " ${description}")) args-list))
+        )
+      )
+    )
+  )
+
+(defun insert_rdoc_comments_for_arguments ()
+  (save-excursion
+    (re-search-forward "def \\([a-zA-Z_0-9.?!]+\\)" nil t)
+    (match-data)
+    (let ((method-name (buffer-substring (match-beginning 1) (match-end 1)))
+          ;; this assumes that ruby methods are defined on a single line
+          (arguments-string (buffer-substring (match-end 1) (line-end-position))))
+      (let ((args-list
+             (remove-if
+              (lambda (x) (string= x ""))
+              (mapcar
+               (lambda (arg) (car (split-string (replace-regexp-in-string "[ ()]" "" arg) "=")))
+               (split-string arguments-string ",")))))
+        (apply 'concat (mapcar (lambda (arg) (concat "#  " arg "<>::\n#    ${description}\n")) args-list))
+        )
+      )
+    )
+  )
