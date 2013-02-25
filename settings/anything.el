16b5c7a9a2cf3f60e1b32bfb49fd2a6feecaf1e9
John Le <moonpiesandbox@gmail.com>
2013-02-25 15:42:23 -0800

    setting sources to search with anything

---
+++ settings/anything.el
@@ -0,0 +1,35 @@
+(defvar anything-c-source-git-project-files-cache nil "(path signature cached-buffer)")
+(defvar anything-c-source-git-project-files
+  '((name . "Files from Current GIT Project")
+    (init . (lambda ()
+              (let* ((top-dir (file-truename (magit-get-top-dir (if (buffer-file-name)
+                                                                    (file-name-directory (buffer-file-name))
+                                                                  default-directory))))
+                     (default-directory top-dir)
+                     (signature (magit-rev-parse "HEAD")))
+
+                (unless (and anything-c-source-git-project-files-cache
+                             (third anything-c-source-git-project-files-cache)
+                             (equal (first anything-c-source-git-project-files-cache) top-dir)
+                             (equal (second anything-c-source-git-project-files-cache) signature))
+                  (if (third anything-c-source-git-project-files-cache)
+                      (kill-buffer (third anything-c-source-git-project-files-cache)))
+                  (setq anything-c-source-git-project-files-cache
+                        (list top-dir
+                              signature
+                              (anything-candidate-buffer 'global)))
+                  (with-current-buffer (third anything-c-source-git-project-files-cache)
+                    (dolist (filename (mapcar (lambda (file) (concat default-directory file))
+                                              (magit-git-lines "ls-files")))
+                      (insert filename)
+                      (newline))))
+                (anything-candidate-buffer (third anything-c-source-git-project-files-cache)))))
+
+    (type . file)
+    (candidates-in-buffer)))
+
+(setq anything-sources
+      '(;; anything-c-source-buffers-list
+        anything-c-source-recentf
+        anything-c-source-files-in-current-dir+
+        anything-c-source-git-project-files))
