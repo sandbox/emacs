a6b02747f2e0c26fbd9dfc718933e6893e8bb48c
John Le <le.johnq@gmail.com>
2019-11-04 13:31:42 -0800

    theme changes

--- init.el
+++ init.el
@@ -31,7 +31,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ '(compilation-error-regexp-alist
+   (quote
+    (bash python-tracebacks-and-caml cmake cmake-info comma cucumber msft edg-1 edg-2 epc ftnchek iar ibm irix java jikes-file jikes-line clang-include gcc-include ruby-Test::Unit gnu lcc makepp mips-1 mips-2 msft omake oracle perl php rxp sparc-pascal-file sparc-pascal-line sparc-pascal-example sun sun-ada watcom 4bsd gcov-file gcov-header gcov-nomark gcov-called-line gcov-never-called perl--Pod::Checker perl--Test perl--Test2 perl--Test::Harness weblint guile-file guile-line)))
+ '(package-selected-packages
+   (quote
+    (git-link go-mode zenburn-theme yaml-mode web-mode smart-tab scss-mode scratch scala-mode sass-mode rspec-mode php-mode nose markdown-mode magit lua-mode helm-ls-git haskell-mode groovy-mode gitignore-mode gitconfig-mode flycheck expand-region exec-path-from-shell evil-surround evil-paredit evil-numbers evil-leader diminish csv-mode coffee-mode)))
+ '(sh-basic-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
